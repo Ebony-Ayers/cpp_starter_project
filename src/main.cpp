ec775f3bcae40dac25cb928ae8a712b95e112d14
Joseph Davies <daviesjd1@gmail.com>
2021-03-21 01:35:37 +1100

    customised forked repository

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,12 @@
-#include <functional>
 #include <iostream>
 
-#include <spdlog/spdlog.h>
-#include <docopt/docopt.h>
-
-static constexpr auto USAGE =
-  R"(Naval Fate.
-
-    Usage:
-          naval_fate ship new <name>...
-          naval_fate ship <name> move <x> <y> [--speed=<kn>]
-          naval_fate ship shoot <x> <y>
-          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
-          naval_fate (-h | --help)
-          naval_fate --version
- Options:
-          -h --help     Show this screen.
-          --version     Show version.
-          --speed=<kn>  Speed in knots [default: 10].
-          --moored      Moored (anchored) mine.
-          --drifting    Drifting mine.
-)";
-
-int main(int argc, const char **argv)
+int main(int argc, const char* argv[])
 {
-  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
-    { std::next(argv), std::next(argv, argc) },
-    true,// show help if requested
-    "Naval Fate 2.0");// version string
+	std::cout << "Hello World!" << std::endl;
 
-  for (auto const &arg : args) {
-    std::cout << arg.first << arg.second << std::endl;
-  }
-
-
-  //Use the default logger (stdout, multi-threaded, colored)
-  spdlog::info("Hello, {}!", "World");
-
-  fmt::print("Hello, from {}\n", "{fmt}");
+	//example of compiler warnings
+	int32_t var1 = 0;
+	int64_t var2 = (int64_t)var1;
+	
+	return 0;
 }
