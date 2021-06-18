44f0229807ce99e364aa16d8125393e072df69bd
Ebony Ayers <ebonyayers2718@gmail.com>
2021-06-18 20:01:37 +1000

    removed build

--- build/src/CMakeFiles/test.dir/Unity/unity_0_cxx.cxx.o.d
+++
@@ -1,5 +0,0 @@
-src/CMakeFiles/test.dir/Unity/unity_0_cxx.cxx.o: \
- /home/ebony/git/Ebony-Ayers/cpp_starter_project/build/src/CMakeFiles/test.dir/Unity/unity_0_cxx.cxx \
- /usr/include/stdc-predef.h \
- /home/ebony/git/Ebony-Ayers/cpp_starter_project/src/main.cpp \
- /usr/include/c++/11.1.0/iostream
