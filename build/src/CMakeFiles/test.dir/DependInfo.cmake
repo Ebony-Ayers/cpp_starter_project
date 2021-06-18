fe81d1574b0278aa5187ef7d5eb0b8e7db625ba1
Ebony Ayers <ebonyayers2718@gmail.com>
2021-06-18 19:22:58 +1000

    removed unneeded functionality

---
+++ build/src/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ebony/git/Ebony-Ayers/cpp_starter_project/build/src/CMakeFiles/test.dir/Unity/unity_0_cxx.cxx" "src/CMakeFiles/test.dir/Unity/unity_0_cxx.cxx.o" "gcc" "src/CMakeFiles/test.dir/Unity/unity_0_cxx.cxx.o.d"
+  "/home/ebony/git/Ebony-Ayers/cpp_starter_project/build/src/CMakeFiles/test.dir/cmake_pch.hxx" "src/CMakeFiles/test.dir/Unity/unity_0_cxx.cxx.o" "gcc" "src/CMakeFiles/test.dir/Unity/unity_0_cxx.cxx.o.d"
+  "/home/ebony/git/Ebony-Ayers/cpp_starter_project/build/src/CMakeFiles/test.dir/cmake_pch.hxx.cxx" "src/CMakeFiles/test.dir/cmake_pch.hxx.gch" "gcc" "src/CMakeFiles/test.dir/cmake_pch.hxx.gch.d"
+  "/home/ebony/git/Ebony-Ayers/cpp_starter_project/build/src/CMakeFiles/test.dir/cmake_pch.hxx" "src/CMakeFiles/test.dir/cmake_pch.hxx.gch" "gcc" "src/CMakeFiles/test.dir/cmake_pch.hxx.gch.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
